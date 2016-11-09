508ecb33bec80bd9bb49550e2cde4c0c76f0af73
JessicaSung <jessica.sung@utexas.edu>
2016-11-09 09:54:39 -0600

    Completed connection.js file. Started orm.js file.

---
+++ config/connection.js
@@ -0,0 +1,21 @@
+// connection to the database exported to be used by ORM
+
+var sqlPassword = require('./password.js');
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+	port: 3306,
+	host: 'localhost',
+	user: 'root',
+	password: sqlPassword,
+	database: 'burgers_db'
+});
+
+connection.connect(function (err) {
+	if (err) {
+		console.error('error connecting: ' + err.stack);
+		return;
+	}
+	console.log('connected as id ' + connection.threadId);
+});
+
+module.exports = connection;
---
+++ config/orm.js
@@ -0,0 +1,5 @@
+// Import (require) connection.js. Create the methods that will execute 
+// the necessary MySQL commands in the controllers.
+
+var connection = require('../config/connection.js');
+
