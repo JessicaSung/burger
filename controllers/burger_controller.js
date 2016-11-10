dd9801f6239d3eb25c244da6bc1c3d46f6908643
JessicaSung <jessica.sung@utexas.edu>
2016-11-09 19:16:35 -0600

    Completed controllers/burger_controller.js logic

---
+++ controllers/burger_controller.js
@@ -0,0 +1,48 @@
+// Import (require) burger.js
+// =============================================================================
+var burger = require('../models/burger.js');
+var express = require('express');
+
+
+// Create app router
+// =============================================================================
+var router = express.Router();
+
+
+// GET - selectAll
+router.get('/', function (req, res) {
+	res.redirect('/burgers');
+});
+
+router.get('/burgers', function (req, res) {
+	burger.selectAll(function (data) {
+		var hbsObject = { burgers: data };
+		console.log(hbsObject);
+		res.render('index', hbsObject);
+	});
+});
+
+
+// POST - insertOne
+router.post('/burgers/create', function (req, res) {
+	burger.insertOne(['burger_name', 'devoured'], [req.body.burger_name, req.body.devoured], function () {
+		res.redirect('/burgers');
+	});
+});
+
+
+// PUT - updateOne
+router.put('/burgers/update/:id', function (req, res) {
+	var condition = 'id = ' + req.params.id;
+
+	console.log('condition', condition);
+
+	burger.updateOne({ devoured: req.body.devoured }, condition, function () {
+		res.redirect('/burgers');
+	});
+});
+
+
+// Export router
+// =============================================================================
+module.exports = router;
