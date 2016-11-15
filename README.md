945979dbfdac6a917e1972c7a7e0983a4969e8c5
JessicaSung <jessica.sung@utexas.edu>
2016-11-14 22:49:49 -0600

    Added process.env.JAWSDB_URL connection details to connection.js

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # burger
 Week 14, Homework 12, Eat-Da-Burger!
-
-At this point in the course, I do not feel comfortable with the technologies used to create this application. I completed it with the help of in-class activities.
--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,21 @@
 // connection to the database exported to be used by ORM
-
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-	port: 3306,
-	host: 'localhost',
-	user: 'root',
-	password: '',
-	database: 'burgers_db'
-});
+var  connection;
+
+
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);	
+}
+else {
+	var connection = mysql.createConnection({
+		port: 3306,
+		host: 'localhost',
+		user: 'root',
+		password: '',
+		database: 'burgers_db'
+	});
+}
+
 
 connection.connect(function (err) {
 	if (err) {
@@ -16,5 +24,6 @@ connection.connect(function (err) {
 	}
 	console.log('connected as id ' + connection.threadId);
 });
+
 
 module.exports = connection;
